7355873b0d7323ea5ff32f06b3f7345888072211
iqans <iqan.shaikh@gmail.com>
2019-03-23 21:11:24 +0530

    docker files added

---
+++ src/CreateDb.sql
@@ -0,0 +1,9 @@
+﻿create database DummyDatabase;
+Go;
+
+create table MessageData
+(
+MessageDataId int IDENTITY(1,1) primary key,
+MessageText varchar(200) not null
+);
+Go;
---
+++ src/db.sh
@@ -0,0 +1,10 @@
+echo "Setting Environment variables."
+export ACCEPT_EULA=Y
+export SA_PASSWORD=SuperSecretPassword#1
+echo "Environment variables set."
+echo "Starting SqlServr"
+/opt/mssql/bin/sqlservr &
+sleep 60 | echo "Waiting for 60s to start Sql Server"
+echo "Restoring DB."
+/opt/mssql-tools/bin/sqlcmd -U sa -P SuperSecretPassword#1 -i $1
+echo "DB restored."
