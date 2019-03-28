4b9549a9ae8de50cc10b1fc3e7ac0a3043053226
iqans <iqan.shaikh@gmail.com>
2019-03-28 18:58:40 +0000

    small changes

--- src/CreateDb.sql
+++ src/CreateDb.sql
@@ -1,9 +1,9 @@
 ﻿create database DummyDatabase;
-Go;
+
+use DummyDatabase;
 
 create table MessageData
 (
 MessageDataId int IDENTITY(1,1) primary key,
 MessageText varchar(200) not null
-);
-Go;
+)
